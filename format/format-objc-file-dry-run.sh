8bcda907cc952c6f141472661dabf4ca4633017e
Igor Xiang <xiangyaguo@xxx.com>
2020-08-06 11:48:12 +0800

    支持检测增量代码
    (cherry picked from commit 179d95cc853b92caad7d26597d04ecdb5f251216)

--- format/format-objc-file-dry-run.sh
+++ format/format-objc-file-dry-run.sh
@@ -17,8 +17,8 @@ cat "$1" | \
 python "$DIR"/custom/LiteralSymbolSpacer.py | \
 python "$DIR"/custom/InlineConstructorOnSingleLine.py | \
 python "$DIR"/custom/MacroSemicolonAppender.py | \
-python "$DIR"/custom/DoubleNewlineInserter.py | \
-"$DIR"/bin/clang-format-3.8-custom -i -style=file | \
+# python "$DIR"/custom/DoubleNewlineInserter.py | \
+"$DIR"/bin/clang-format-3.8-custom -style=file -lines=$2 | \
 python "$DIR"/custom/GenericCategoryLinebreakIndentation.py | \
 python "$DIR"/custom/ParameterAfterBlockNewline.py | \
 python "$DIR"/custom/HasIncludeSpaceRemover.py | \
