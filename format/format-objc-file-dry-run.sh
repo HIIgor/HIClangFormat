d59c2916e5dc0e3d768e21cb342e4ceeac49f7cd
Igor Xiang <xiangyaguo@xxx.com>
2020-08-07 23:21:31 +0800

    shell数组拼接cmd,检测changed lines
    (cherry picked from commit 2308973f3067f74df780e4b57a249289c03b9b0a)

--- format/format-objc-file-dry-run.sh
+++ format/format-objc-file-dry-run.sh
@@ -8,19 +8,50 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # "#pragma Formatter Exempt" or "// MARK: Formatter Exempt" means don't format this file.
 # Read the first line and trim it.
-line="$(head -1 "$1" | xargs)" 
+line="$(head -1 "$1" | xargs)"
 if [ "$line" == "#pragma Formatter Exempt" -o "$line" == "// MARK: Formatter Exempt" ]; then
   cat "$1" && exit 0
 fi
 
-cat "$1" | \
-python "$DIR"/custom/LiteralSymbolSpacer.py | \
-python "$DIR"/custom/InlineConstructorOnSingleLine.py | \
-python "$DIR"/custom/MacroSemicolonAppender.py | \
-# python "$DIR"/custom/DoubleNewlineInserter.py | \
-"$DIR"/bin/clang-format-3.8-custom -style=file -lines=$2 | \
-python "$DIR"/custom/GenericCategoryLinebreakIndentation.py | \
-python "$DIR"/custom/ParameterAfterBlockNewline.py | \
-python "$DIR"/custom/HasIncludeSpaceRemover.py | \
-python "$DIR"/custom/NewLineAtEndOfFileInserter.py
+file=$1
+changed_lines=$2
 
+
+commands[0]="| python $DIR/custom/LiteralSymbolSpacer.py"
+commands[${#commands[@]}]="| python \"$DIR\"/custom/InlineConstructorOnSingleLine.py"
+commands[${#commands[@]}]="| python \"$DIR\"/custom/MacroSemicolonAppender.py"
+
+
+for line in ${changed_lines[@]}; do
+	commands[${#commands[@]}]="| $DIR/bin/clang-format-3.8-custom -style=file -lines=$line"
+done
+
+commands[${#commands[@]}]="| python \"$DIR\"/custom/GenericCategoryLinebreakIndentation.py"
+commands[${#commands[@]}]="| python \"$DIR\"/custom/ParameterAfterBlockNewline.py"
+commands[${#commands[@]}]="| python \"$DIR\"/custom/HasIncludeSpaceRemover.py"
+commands[${#commands[@]}]="| python \"$DIR\"/custom/NewLineAtEndOfFileInserter.py"
+
+
+cmd="cat $file"
+for var in ${commands[@]}; do
+	cmd="$cmd $var"
+done
+eval $cmd
+
+
+
+
+#origin script as blew
+# cat $file | \
+# python "$DIR"/custom/LiteralSymbolSpacer.py | \
+# python "$DIR"/custom/InlineConstructorOnSingleLine.py | \
+# python "$DIR"/custom/MacroSemicolonAppender.py | \
+
+# "$DIR"/bin/clang-format-3.8-custom -style=file -lines=50:56 | \
+# "$DIR"/bin/clang-format-3.8-custom -style=file -lines=70:76 | \
+# "$DIR"/bin/clang-format-3.8-custom -style=file -lines=86:93 | \
+
+# python "$DIR"/custom/GenericCategoryLinebreakIndentation.py | \
+# python "$DIR"/custom/ParameterAfterBlockNewline.py | \
+# python "$DIR"/custom/HasIncludeSpaceRemover.py | \
+# python "$DIR"/custom/NewLineAtEndOfFileInserter.py
