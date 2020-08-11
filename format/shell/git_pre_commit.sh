03446eb953b3c8cb098a51e457c294661162745a
Igor Xiang <xiangyaguo@xxx.com>
2020-08-11 14:21:29 +0800

    将git_hook相关操作分离出去
    (cherry picked from commit ee00ac6ac6c22520cf2a5f6bb44da54f8d547ece)

--- format/shell/git_pre_commit.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-current_repo_path=$(git rev-parse --show-toplevel)
-if [[ -e $current_repo_path/format/format-objc-hook ]]; then
-   $current_repo_path/format/format-objc-hook || exit 1;
-fi
-
